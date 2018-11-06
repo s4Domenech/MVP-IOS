ff4438a1e75bc4601b2d976b14a7f2a3ccf3b165
Sylvia <scallejas@fidesol.org>
2018-11-06 17:42:35 +0100

    Map in progress..

---
+++ MVP/View/MapViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MapViewController.swift
+//  MVP
+//
+//  Created by Sylvia Callejas on 06/11/2018.
+//  Copyright © 2018 Sylvia Callejas. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
